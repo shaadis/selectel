6c32ad58d7dd36714f47310311af0c5bf3e70839
olga <moonsatoko@gmail.com>
2016-07-19 13:15:20 +0300

    store numbers of submits in local storage

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -5,18 +5,29 @@
     var submit = document.getElementById('submit');
     var form = document.getElementById('form-congratulate');
 
+
     form.addEventListener('submit', function(e) {
         e.preventDefault();
-        var form = e.target;
+        //counting submits
+        var congratCount = localStorage.getItem('trexCongrats');
+        if(congratCount === null) {
+            congratCount = 0;
+        }
+        congratCount++;
+        localStorage.setItem('trexCongrats', congratCount);
+
+        //send form
         var request = new XMLHttpRequest();
-        var data = new FormData(form);
-        request.open(form.method, form.action);
-        request.send(data);
+        request.open(form.method, form.action, true);
+
+
         request.onload = function(){
             var resp = null;
             if (request.status >= 200 && request.status < 400) {
                 resp = request.responseText;
                 form.innerHTML = resp;
+                var trexCongratContainer = document.getElementById('congrats-count');
+                trexCongratContainer.innerText = localStorage.getItem('trexCongrats');
             } else {
                 console.log('wrong request');
             }
@@ -24,6 +35,7 @@
         request.onerror = function() {
             console.log('problems of some sort');
         };
+        request.send();
     });
 }();
 
