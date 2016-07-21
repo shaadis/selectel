285dd0d9b26ae1eafbf0578803b64e9b530a51ca
olga <moonsatoko@gmail.com>
2016-07-21 15:12:10 +0300

    modal - add/remove class on body

--- src/assets/js/modal.js
+++ src/assets/js/modal.js
@@ -2,13 +2,19 @@
     var modal = document.getElementById('modal');
     var modalOpenBtn = document.getElementById('modal-open');
     var closeModalBtn = document.querySelectorAll('.js-close-modal');
+    var docBody = document.body;
     modalOpenBtn.addEventListener('click', function() {
         modal.style.display = 'block';
+        if (docBody.classList)
+            docBody.classList.add('is-modal-open');
+        else
+            docBody.className += ' is-modal-open';
     });
     //todo close modal by clicking overlay
     [].forEach.call(closeModalBtn, function(elem) {
         elem.addEventListener('click', function() {
             modal.style.display = 'none';
+            docBody.classList.remove('is-modal-open');
         });
     });
 }();
