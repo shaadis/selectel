e2d2d634a74fa912ffedcfcc1e9a0f2776bdcf95
Olga Ryabenko <moonsatoko@gmail.com>
2016-07-24 18:02:14 +0300

    slider updates

--- src/assets/js/modal.js
+++ src/assets/js/modal.js
@@ -1,20 +1,17 @@
-;!function() {
-    var modal = document.getElementById('modal');
-    var modalOpenBtn = document.getElementById('modal-open');
-    var closeModalBtn = document.querySelectorAll('.js-close-modal');
-    var docBody = document.body;
-    modalOpenBtn.addEventListener('click', function() {
-        modal.style.display = 'block';
-        if (docBody.classList)
-            docBody.classList.add('is-modal-open');
-        else
-            docBody.className += ' is-modal-open';
+;!function () {
+  var modal = document.getElementById('modal');
+  var modalOpenBtn = document.getElementById('modal-open');
+  var closeModalBtn = document.querySelectorAll('.js-close-modal');
+  var docBody = document.body;
+  modalOpenBtn.addEventListener('click', function () {
+    modal.style.display = 'block';
+      addClass(docBody, 'is-modal-open');
+  });
+  //todo close modal by clicking overlay
+  [].forEach.call(closeModalBtn, function (elem) {
+    elem.addEventListener('click', function () {
+      modal.style.display = 'none';
+      removeClass(docBody, 'is-modal-open');
     });
-    //todo close modal by clicking overlay
-    [].forEach.call(closeModalBtn, function(elem) {
-        elem.addEventListener('click', function() {
-            modal.style.display = 'none';
-            docBody.classList.remove('is-modal-open');
-        });
-    });
+  });
 }();
---
+++ src/assets/js/util/util.js
@@ -0,0 +1,13 @@
+function addClass(element, className) {
+  if (element.classList)
+    element.classList.add(className);
+  else
+    element.className += ' ' + className;
+}
+
+function removeClass(element, className) {
+  if (element.classList)
+    element.classList.remove(className);
+  else
+    element[className] = element[className].replace(new RegExp('(^|\\b)' + className.split(' ').join('|') + '(\\b|$)', 'gi'), ' ');
+}
